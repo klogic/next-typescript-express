6d64db926a7a55e03f571466912c21513bd18d1e
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-12-16 22:39:26 +0700

    standard server.tsx

---
+++ server.tsx
@@ -0,0 +1,24 @@
+var express = require('express');
+
+const next = require('next');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const app = next({ isDev });
+const handle = app.getRequestHandler();
+const server = express();
+
+app
+  .prepare()
+  .then(() => {
+    server.get('*', (req: Request, res: Response) => {
+      return handle(req, res);
+    });
+    server.listen(3000, (err) => {
+      if (err) throw err;
+      console.log('server ready on port 3000');
+    });
+  })
+  .catch((exception) => {
+    console.error(exception.stack);
+    process.exit(1);
+  });
